4d5d59ba5742f97df4e2b8aeacbd8f8a0fa55914
hexaalif <hexaalif2020@gamil.com>
2022-04-18 22:30:04 +0600

    login authentication added with error msg

--- src/Component/Pages/Login/Login/Login.js
+++ src/Component/Pages/Login/Login/Login.js
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import auth from "../../../../firebase.init";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import MediaLogin from "../MediaLogin/MediaLogin";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const location = useLocation();
+  let from = location.state?.from?.pathname || "/";
+
   let navigate = useNavigate();
-  const handleLogIn = () => {
-    navigate("/home");
+  const handleLogIn = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    signInWithEmailAndPassword(email, password);
   };
+
+  let errorElement;
+  if (error) {
+    errorElement = <p className="text-danger">Error: {error?.message} </p>;
+  }
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
   return (
     <div>
       <h1 className="text-center text-dark fw-bolder my-5">Login</h1>
       <Form onSubmit={handleLogIn} className="w-50 d-block m-auto login-form">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
+        {errorElement}
         <Button variant="primary" type="submit">
           Login
         </Button>
